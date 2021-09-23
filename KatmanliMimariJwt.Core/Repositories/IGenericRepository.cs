98592b54950347bc77ff24466bfaca33dc29144e
Salih AVCI <cacabeystjr@gmail.com>
2021-09-23 17:23:47 +0300

    N-Layer Architecture Added

---
+++ KatmanliMimariJwt.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatmanliMimariJwt.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        //Where içerisindeki parametre bir delegate'dir TEntity verip Bool döndürecek
+        IQueryable<TEntity> Where(Expression<Func<TEntity,bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
---
+++ SharedLibrary/DTOs/ErrorDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.DTOs
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(string Error,bool isShow)
+        {
+            Errors.Add(Error);
+            isShow = true;
+        }
+        public ErrorDto(List<string> Errors,bool isShow)
+        {
+            this.Errors = Errors;
+            IsShow = isShow;
+        }
+    }
+}
