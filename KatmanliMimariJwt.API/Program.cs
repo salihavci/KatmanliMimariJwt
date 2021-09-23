32c6c9ac573e735d7a046672a420cf74495a39da
Salih AVCI <cacabeystjr@gmail.com>
2021-09-23 16:21:27 +0300

    API Initial

--- KatmanliMimariJwt.API/Program.cs
+++ KatmanliMimariJwt.API/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace KatmanliMimatiJwt
+namespace KatmanliMimariJwt.API
 {
     public class Program
     {
